68f8d6b16ea2dc65f5fee865f5bcb4e3a9753324
Balázs Pete <bazsip@gmail.com>
2013-10-04 19:16:50 +0100

    Reflect modifications of Message in TextMessage

--- src/communication/messages/TextMessage.java
+++ src/communication/messages/TextMessage.java
@@ -2,10 +2,20 @@ package communication.messages;
 
 public class TextMessage extends Message {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7996405491053757702L;
 	protected String message = null;
 	
+	protected final String messageType = "TextMessage";
+	
+	public TextMessage(String message) {
+		this.message = message;
+	}
+	
 	@Override
-	public String getContents() {
+	public Object getContents() {
 		return message;
 	}
 
