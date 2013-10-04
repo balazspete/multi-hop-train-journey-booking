221a0a9aff648375b2548bb4b53d73b9dc1b0ead
Balázs Pete <bazsip@gmail.com>
2013-10-04 20:27:01 +0100

    Document ServerNetworkingInterface

--- src/communication/ServerNetworkingInterface.java
+++ src/communication/ServerNetworkingInterface.java
@@ -2,29 +2,59 @@ package communication;
 
 import java.util.HashMap;
 
-import communication.protocols.Protocol;
-import communication.messages.Message;
+import communication.protocols.*;
+import communication.messages.*;
 
+/**
+ * A general server networking interface
+ * @author Balazs Pete
+ *
+ */
 public abstract class ServerNetworkingInterface extends Thread {
 
 	protected HashMap<String, Protocol> protocolMap = new HashMap<String, Protocol>();
 	
+	/**
+	 * Start accepting incoming connections and messages
+	 * @throws CommunicationException Thrown in case of an error
+	 */
 	public abstract void acceptConnections() throws CommunicationException;
 	
-	public synchronized void addProtocol(Protocol protocol) {
+	/** 
+	 * Add a Protocol to handle a certain type of message, if a previous protocol with the same message type has been added, it will be replaced
+	 * @param protocol The Protocol to add
+	 */
+	public synchronized void putProtocol(Protocol protocol) {
 		protocolMap.put(protocol.getAcceptedMessageType(), protocol);
 	}
 	
+	/**
+	 * Remove a specific protocol from the interface
+	 * @param protocol The protocol to remove
+	 */
 	public synchronized void removeProtocol(Protocol protocol) {
 		protocolMap.remove(protocol.getAcceptedMessageType());
 	}
 	
+	/**
+	 * Remove all protocols from the interface
+	 */
 	public void removeAllProtocols() {
 		protocolMap = new HashMap<String, Protocol>();
 	}
 
-	protected Message processMessage(Message message) {
+	/**
+	 * Process an input message using one of the input protocols
+	 * @param message The message to process
+	 * @return The response message
+	 * @throws InvalidMessageException Thrown in case the input message cannot be handled by the interface
+	 */
+	protected Message processMessage(Message message) throws InvalidMessageException {
 		Protocol p = protocolMap.get(message.getType()); 
+		
+		if(p == null) 
+			throw new InvalidMessageException("Message type cannot be handled by the interface");
+		
 		return p.processMessage(message);
 	}
 	
---
+++ src/communication/messages/InvalidMessageException.java
@@ -0,0 +1,9 @@
+package communication.messages;
+
+public class InvalidMessageException extends Exception {
+
+	public InvalidMessageException(String message) {
+		super(message);
+	}
+	
+}
