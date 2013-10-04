6d7e123abfe472f701492301ad418a74be1d36e1
Balázs Pete <bazsip@gmail.com>
2013-10-04 21:02:34 +0100

    Document Protocol(s) and related exceptions

--- src/communication/messages/InvalidMessageException.java
+++ src/communication/messages/InvalidMessageException.java
@@ -1,9 +1,17 @@
 package communication.messages;
 
+/**
+ * An exception to be thrown when a protocol does not support the input 
+ * @author Balazs Pete
+ *
+ */
 public class InvalidMessageException extends Exception {
 
+	/**
+	 * Create a new InvalidMessageException
+	 * @param message The message describing the exception
+	 */
 	public InvalidMessageException(String message) {
 		super(message);
 	}
-	
 }
