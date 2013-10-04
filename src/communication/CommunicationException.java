55fcb0b83e5ddfd95d6a80308aeba7e10d0ecdc1
Balázs Pete <bazsip@gmail.com>
2013-10-04 19:17:49 +0100

    Add network interface implementations using socket communication
    Include a simple Loopback protocol for testing

--- src/communication/CommunicationException.java
+++ src/communication/CommunicationException.java
@@ -7,4 +7,8 @@ public class CommunicationException extends Exception {
 	 */
 	private static final long serialVersionUID = -1153395142101460769L;
 
+	public CommunicationException(String message) {
+		super(message);
+	}
+	
 }
